f7b6d076c4667b53319e02911c806e12ddefee80
Jeremie <jeremie.de-pas@epitech.eu>
2018-03-04 23:04:05 +0100

    js porte

--- app_led/static/js/home.js
+++ app_led/static/js/home.js
@@ -1,20 +1,22 @@
 namespace = '/'
 global_socket = io.connect('http://' + document.domain + ':' + location.port + namespace);
 enigme_socket = io('/enigme');
+porte_socket  = io('/porte');
 
 function enigme(id, action, auto = "true"){
-  enigme_socket.emit(id, {action: action, auto: auto})
+  porte_socket.emit(id, {action: action, auto: auto})
 }
 
 function porte(id, action, auto = "true") {
-  enigme_socket.emit("porte", {id: id, action: action, auto: auto})
+  porte_socket.emit("porte", {id: id, action: action, auto: auto})
 }
 
 function ledemit(id, value) {
   led_socket.emit("led", {id: id, valiue: value})
 }
 function boucle(){
-  enigme_socket.emit("info", 'test')
+  enigme_socket.emit("info_enigme", 'test')
+  porte_socket.emit("info_porte", 'test')
   setTimeout("boucle()",1000);
 }
 
@@ -43,15 +45,29 @@ window.onload = function(){
         classenigme[i].add("green");
       }
     }
-
   });
-
-    // $('form#echo').submit(function(event){
-    //     test_socket.emit('echo', {data: $('#message').val()});
-    //     return false;
-    // });
-    // document.getElementsByName("enigme-start").addEventListener("click", enigmestart);
-    // document.getElementsByName("enigme-stop").addEventListener("click", enigmestop);
-    // document.getElementsByName("enigme-mode").addEventListener("click", enigmemode);
-    // document.getElementsByName("enigme-reload").addEventListener("click", enigmerelod);
+  porte_socket.on('info_porte', function(str){
+    data = JSON.parse(str);
+    classenigme = []
+    classenigme[0] = document.getElementById('porte_0').classList
+    classenigme[1] = document.getElementById('porte_1').classList
+    classenigme[2] = document.getElementById('porte_2').classList
+    classenigme[3] = document.getElementById('porte_3').classList
+    for (var i = 0; i < classenigme.length; i++) {
+      console.log(data.enigme[i])
+      if(data.enigme[i] == 1){
+        classenigme[i].add("green");
+        classenigme[i].remove("grey");
+        classenigme[i].remove("red");
+      } else if (data.enigme[i] == 0) {
+        classenigme[i].add("red");
+        classenigme[i].remove("grey");
+        classenigme[i].remove("green");
+      } else {
+        classenigme[i].remove("red");
+        classenigme[i].remove("grey");
+        classenigme[i].add("green");
+      }
+    }
+  });
 }
