d51c0b627424c961c82e18cb5a0f7fd350c58bc9
Jeremie <jeremie.de-pas@epitech.eu>
2018-03-04 22:19:29 +0100

    test object

---
+++ app_led/api_i2c/porte_0.py
@@ -0,0 +1,34 @@
+import smbus
+import pipes
+import time
+
+class Porte():
+    def __init__():
+        self.bus = smbus.SMBus(1)
+        self.porte_0  = 0x11 # trape apres enigme_1
+        with t.open('pipes/porte_0', 'w') as f:
+            f.write('0--True--') # valeur,  mode-aut, valeur-enigme
+    def porte_0():
+        with t.open('pipes/porte_0', 'r') as f:
+            command = f.read().split('--')
+            print(command)
+        if command[1] == "True":
+            GPIO.output(12, readNumber(address0))
+            with t.open('pipes/porte_0', 'w') as f:
+                f.write(str(readNumber(address0))+"--True--"+str(command[2]))
+        if command[1] == "false":
+            GPIO.output(12, int(command[0]))
+
+
+def writeNumber(address, value):
+    try:
+        bus.write_byte(address, value)
+        return 1
+    except OSError as e:
+        return -1
+def readNumber(address):
+    try:
+        number = bus.read_byte(address)
+        return number
+    except OSError as e:
+        return -1
--- app_led/gestion_enigme.py
+++ app_led/gestion_enigme.py
@@ -3,13 +3,14 @@ import smbus
 import pipes
 import time
 import RPi.GPIO as GPIO
-import api_i2c.enigme as enigme_3
+import api_i2c.porte_0 as Porte
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(12, GPIO.OUT)
 
 # ser = serial.Serial('/dev/ttyUSB0', 115200)
 t = pipes.Template()
+porte_0 = Porte.Porte()
 
 bus = smbus.SMBus(1)
 address0 = 0x05
@@ -17,7 +18,7 @@ address1 = 0x06
 address2 = 0x07
 address3 = 0x08
 
-porte_0  = 0x11 # trape apres enigme_1
+# porte_0  = 0x11 # trape apres enigme_1
 porte_1  = 0x12 # trape clavier lazer
 porte_2  = 0x13 # porte salle 1 -> salle 2
 porte_3  = 0x14 # porte salle 2 -> salle 3
@@ -34,9 +35,74 @@ with t.open('pipes/enigme_2', 'w') as f:
 with t.open('pipes/enigme_3', 'w') as f:
     f.write('0--True--false') # valeur,  mode-auto, restart enigme
 
+with t.open('pipes/porte_0', 'w') as f:
+    f.write('0--True') #valeur,  mode-auto
+with t.open('pipes/porte_1', 'w') as f:
+    f.write('0--True') #valeur,  mode-auto
+with t.open('pipes/porte_2', 'w') as f:
+    f.write('0--True') #valeur,  mode-auto
+with t.open('pipes/porte_3', 'w') as f:
+    f.write('0--True--false') # valeur,  mode-auto, restart enigme
+
+
+def writeNumber(address, value):
+    try:
+        bus.write_byte(address, value)
+        return 1
+    except OSError as e:
+        return -1
+def readNumber(address):
+    try:
+        number = bus.read_byte(address)
+        return number
+    except OSError as e:
+        return -1
+
+
+
+def enigme_0():
+    with t.open('pipes/enigme_0', 'r') as f:
+        command = f.read().split('--')
+    if command[1] == "True":
+        GPIO.output(12, readNumber(address0))
+        with t.open('pipes/enigme_0', 'w') as f:
+            f.write(str(readNumber(address0))+"--True")
+    if command[1] == "false":
+        GPIO.output(12, int(command[0]))
+
+def enigme_1():
+    with t.open('pipes/enigme_1', 'r') as f:
+        command = f.read().split('--')
+    if command[1] == "True":
+        GPIO.output(12, readNumber(address1))
+        with t.open('pipes/enigme_1', 'w') as f:
+            f.write(str(readNumber(address1))+"--True")
+    if command[1] == "false":
+        GPIO.output(12, int(command[0]))
+
+def enigme_3():
+    with t.open('pipes/enigme_3', 'r') as f:
+        command = f.read().split('--')
+        print(command)
+        print(readNumber(address3))
+
+    if command[0] == '-1':
+        with t.open('pipes/enigme_3', 'w') as f:
+            f.write(str(readNumber(address3))+"--True--false")
+    else:
+        if command[1] == "True":
+            with t.open('pipes/enigme_3', 'w') as f:
+                f.write(str(readNumber(address3))+"--True--false")
+        if command[1] == "false":
+            writeNumber(address3, int(command[0]))
+        if command[2] == "True":
+            writeNumber(address3, int(2))
+            with t.open('pipes/enigme_3', 'w') as f:
+                f.write(str(readNumber(address3))+"--True--false")
 
 
 while True:
+    porte_0()
     # enigme_0()
     enigme_3()
     porte_0()
