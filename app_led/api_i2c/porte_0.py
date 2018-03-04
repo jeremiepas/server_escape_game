51ac485449dccd6319b598168abae600078a829a
Jeremie <jeremie.de-pas@epitech.eu>
2018-03-04 22:27:07 +0100

    x

--- app_led/api_i2c/porte_0.py
+++ app_led/api_i2c/porte_0.py
@@ -4,18 +4,19 @@ import time
 
 class Porte:
     def __init__(self):
+        self.t = pipes.Template()
         self.bus = smbus.SMBus(1)
         self.porte_0  = 0x11 # trape apres enigme_1
-        with t.open('pipes/porte_0', 'w') as f:
+        with self.t.open('pipes/porte_0', 'w') as f:
             f.write('0--True--') # valeur,  mode-aut, valeur-enigme
     def porte_0():
-        with t.open('pipes/porte_0', 'r') as f:
+        with self.t.open('pipes/porte_0', 'r') as f:
             command = f.read().split('--')
             print(command)
         if command[1] == "True":
-            GPIO.output(12, readNumber(address0))
+            GPIO.output(12, readNumber(self.porte_0))
             with t.open('pipes/porte_0', 'w') as f:
-                f.write(str(readNumber(address0))+"--True--"+str(command[2]))
+                f.write(str(readNumber(self.porte_0))+"--True--"+str(command[2]))
         if command[1] == "false":
             GPIO.output(12, int(command[0]))
 
